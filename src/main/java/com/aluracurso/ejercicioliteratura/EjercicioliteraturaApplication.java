df52f9c652e2af6765be8abc5b5e7cf3452abe97
Mauricio Pulgarin <mapulgarin2@gmail.com>
2025-06-13 06:33:02 -0500

    commit iniciado

---
+++ src/main/java/com/aluracurso/ejercicioliteratura/EjercicioliteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.aluracurso.ejercicioliteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioliteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioliteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracurso/ejercicioliteratura/EjercicioliteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracurso.ejercicioliteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioliteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
